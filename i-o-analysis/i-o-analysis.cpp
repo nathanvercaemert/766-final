51d9c8ece713f633e60f4d57b08f38e868420ef2
Your Name <you@example.com>
2023-04-28 16:43:17 +0000

    trying to fix ast print

--- i-o-analysis/i-o-analysis.cpp
+++ i-o-analysis/i-o-analysis.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include <utility>
-#include <sstream>
-#include <cstring>
 #include <fstream>
+#include <functional>
 #include <clang-c/Index.h>
 
-enum CXChildVisitResult getChildCursorVisitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
-    CXCursor *child_cursor = reinterpret_cast<CXCursor*>(client_data);
-    *child_cursor = cursor;
-    return CXChildVisit_Break; // Stop visiting after the first child
+void traverse_ast(CXCursor cursor, int depth, const std::function<void(CXCursor, int)>& process) {
+    process(cursor, depth);
+    clang_visitChildren(cursor, [](CXCursor c, CXCursor parent, CXClientData client_data) {
+        auto context = reinterpret_cast<std::pair<int, const std::function<void(CXCursor, int)>*>*>(client_data);
+        traverse_ast(c, context->first + 1, *context->second);
+        return CXChildVisit_Continue;
+    }, &std::make_pair(depth, &process));
 }
 
-std::string get_surrounding_context(CXSourceLocation location, int before, int after) {
+std::string get_context(CXSourceLocation location, int before, int after) {
     unsigned line_number;
     CXFile file;
     clang_getSpellingLocation(location, &file, &line_number, nullptr, nullptr);
@@ -30,66 +29,6 @@ std::string get_surrounding_context(CXSourceLocation location, int before, int a
     return context;
 }
 
-// std::string get_function_name(CXCursor cursor) {
-//     CXCursor parent =  clang_getCursorSemanticParent(cursor);
-//     int test = 0;
-//     while (clang_getCursorKind(parent) != CXCursor_FunctionDecl &&
-//            clang_getCursorKind(parent) != CXCursor_CXXMethod &&
-//            test < 100) {
-//         CXString test_parent_name = clang_getCursorKindSpelling(clang_getCursorKind(parent));
-//         std::cout << clang_getCString(test_parent_name) << std::endl;
-//         parent = clang_getCursorSemanticParent(parent);
-//         test ++;
-//     }
-//     CXString parent_name = clang_getCursorSpelling(parent);
-//     std::string function_name(clang_getCString(parent_name));
-//     clang_disposeString(parent_name);
-//     return function_name;
-// }
-
-std::vector<std::pair<std::string, std::pair<std::string, std::string>>> variables_and_context_in_functions;
-
-void print_ast(CXCursor cursor, int depth) {
-    if (clang_getCursorKind(cursor) == CXCursor_FirstInvalid)
-        return;
-
-    for (int i = 0; i < depth; ++i)
-        std::cout << "  ";
-
-    CXString cursor_kind_name = clang_getCursorKindSpelling(clang_getCursorKind(cursor));
-    CXString cursor_spelling = clang_getCursorSpelling(cursor);
-    std::cout << clang_getCString(cursor_kind_name) << " " << clang_getCString(cursor_spelling) << std::endl;
-
-    if (clang_getCursorKind(cursor) == CXCursor_CallExpr && clang_getCString(cursor_spelling) == std::string("scanf")) {
-
-        std::string surrounding_context = get_surrounding_context(clang_getCursorLocation(cursor), 1, 1);
-
-        // std::string function_name = get_function_name(cursor);
-        std::string function_name = "TODO";
-
-        CXCursor arg_cursor = clang_Cursor_getArgument(cursor, 1);
-        if (clang_getCursorKind(arg_cursor) == CXCursor_UnaryOperator) {
-            clang_visitChildren(arg_cursor, getChildCursorVisitor, &arg_cursor);
-        }
-        CXString arg_spelling = clang_getCursorDisplayName(arg_cursor);
-        CXType arg_type = clang_getPointeeType(clang_getCursorType(arg_cursor));
-        
-        std::string var_name(clang_getCString(arg_spelling));
-        variables_and_context_in_functions.emplace_back(var_name, std::make_pair(function_name, surrounding_context));
-    }
-    
-    clang_disposeString(cursor_kind_name);
-    clang_disposeString(cursor_spelling);
-
-    clang_visitChildren(cursor,
-                        [](CXCursor c, CXCursor parent, CXClientData client_data) {
-                            int *depth = reinterpret_cast<int*>(client_data);
-                            print_ast(c, *depth + 1);
-                            return CXChildVisit_Continue;
-                        },
-                        &depth);
-}
-
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Usage: ast_printer <path_to_c_file>" << std::endl;
@@ -97,7 +36,6 @@ int main(int argc, char** argv) {
     }
 
     CXIndex index = clang_createIndex(0, 0);
-    
     const char *args[] = {argv[1], "-std=c11"};
     CXTranslationUnit tu = clang_createTranslationUnitFromSourceFile(index, nullptr, 2, args, 0, nullptr);
 
@@ -107,13 +45,15 @@ int main(int argc, char** argv) {
     }
 
     CXCursor root_cursor = clang_getTranslationUnitCursor(tu);
-    print_ast(root_cursor, 0);
-
-    for (const auto& var_and_context_in_func : variables_and_context_in_functions) {
-        std::cout << "Variable: " << var_and_context_in_func.first << std::endl;
-        std::cout << "Function: " << var_and_context_in_func.second.first << std::endl;
-        std::cout << "Context:" << std::endl << var_and_context_in_func.second.second << std::endl;
-    }
+    traverse_ast(root_cursor, 0, [](CXCursor cursor, int depth) {
+        for (int i = 0; i < depth; ++i)
+            std::cout << "  ";
+        CXString cursor_kind_name = clang_getCursorKindSpelling(clang_getCursorKind(cursor));
+        CXString cursor_spelling = clang_getCursorSpelling(cursor);
+        std::cout << clang_getCString(cursor_kind_name) << " " << clang_getCString(cursor_spelling) << std::endl;
+        clang_disposeString(cursor_kind_name);
+        clang_disposeString(cursor_spelling);
+    });
 
     clang_disposeTranslationUnit(tu);
     clang_disposeIndex(index);
