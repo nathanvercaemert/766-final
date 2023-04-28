ccb53a3aeab31697d8a161920761210da07b0615
Your Name <you@example.com>
2023-04-28 19:27:48 +0000

    got everything i needJ

--- i-o-analysis/i-o-analysis.cpp
+++ i-o-analysis/i-o-analysis.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <utility>
+#include <sstream>
+#include <cstring>
 #include <fstream>
-#include <functional>
 #include <clang-c/Index.h>
 
-void traverse_ast(CXCursor cursor, int depth, const std::function<void(CXCursor, int)>& process) {
-    process(cursor, depth);
-    clang_visitChildren(cursor, [](CXCursor c, CXCursor parent, CXClientData client_data) {
-        auto context = reinterpret_cast<std::pair<int, const std::function<void(CXCursor, int)>*>*>(client_data);
-        traverse_ast(c, context->first + 1, *context->second);
-        return CXChildVisit_Continue;
-    }, &std::make_pair(depth, &process));
+enum CXChildVisitResult getChildCursorVisitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
+    CXCursor *child_cursor = reinterpret_cast<CXCursor*>(client_data);
+    *child_cursor = cursor;
+    return CXChildVisit_Break;
 }
 
-std::string get_context(CXSourceLocation location, int before, int after) {
+std::string get_surrounding_context(CXSourceLocation location, int before, int after) {
     unsigned line_number;
     CXFile file;
     clang_getSpellingLocation(location, &file, &line_number, nullptr, nullptr);
@@ -29,15 +30,73 @@ std::string get_context(CXSourceLocation location, int before, int after) {
     return context;
 }
 
+std::vector<std::pair<std::string, std::string>> variables_and_context;
+
+void print_ast(CXCursor cursor, int depth) {
+    if (clang_getCursorKind(cursor) == CXCursor_FirstInvalid)
+        return;
+
+    for (int i = 0; i < depth; ++i)
+        std::cout << "  ";
+
+    CXString cursor_kind_name = clang_getCursorKindSpelling(clang_getCursorKind(cursor));
+    CXString cursor_spelling = clang_getCursorSpelling(cursor);
+    std::cout << clang_getCString(cursor_kind_name) << " " << clang_getCString(cursor_spelling) << std::endl;
+    
+    clang_disposeString(cursor_kind_name);
+    clang_disposeString(cursor_spelling);
+
+    if (clang_getCursorKind(cursor) == CXCursor_IntegerLiteral) {
+        auto res = clang_Cursor_Evaluate(cursor);
+        auto int_lit_value = clang_EvalResult_getAsInt(res);
+        clang_EvalResult_dispose(res);
+        std::cout << int_lit_value << std::endl;
+    }
+
+    clang_visitChildren(cursor,
+                        [](CXCursor c, CXCursor parent, CXClientData client_data) {
+                            int *depth = reinterpret_cast<int*>(client_data);
+                            print_ast(c, *depth + 1);
+                            return CXChildVisit_Continue;
+                        },
+                        &depth);
+
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Usage: ast_printer <path_to_c_file>" << std::endl;
         return 1;
     }
 
+    // Read the contents of the file into a string.
+    std::ifstream file_stream(argv[1]);
+    std::stringstream buffer;
+    buffer << "#include <stdio.h>\n#include <sys/types.h>\n" << file_stream.rdbuf();
+    std::string file_contents = buffer.str();
+
     CXIndex index = clang_createIndex(0, 0);
-    const char *args[] = {argv[1], "-std=c11"};
-    CXTranslationUnit tu = clang_createTranslationUnitFromSourceFile(index, nullptr, 2, args, 0, nullptr);
+
+    // Create a CXUnsavedFile structure for the modified file.
+    CXUnsavedFile unsaved_file = {
+        argv[1],  // filename
+        file_contents.c_str(),  // contents
+        static_cast<unsigned long>(file_contents.length())  // length
+    };
+
+    const char *args[] = {"-std=c11", "-I/usr/include", "-I/usr/local/include", "-fparse-all-comments"};
+    CXTranslationUnit tu = clang_parseTranslationUnit(
+    index, argv[1], args, 3,
+    &unsaved_file, 1,
+    CXTranslationUnit_DetailedPreprocessingRecord |
+    CXTranslationUnit_IncludeAttributedTypes |
+    CXTranslationUnit_VisitImplicitAttributes |
+    CXTranslationUnit_IgnoreNonErrorsFromIncludedFiles |
+    CXTranslationUnit_KeepGoing |
+    CXTranslationUnit_IncludeBriefCommentsInCodeCompletion |
+    CXTranslationUnit_RetainExcludedConditionalBlocks |
+    CXTranslationUnit_IgnoreNonErrorsFromIncludedFiles
+                                                      );
 
     if (!tu) {
         std::cerr << "Error: Unable to parse the C file." << std::endl;
@@ -45,15 +104,13 @@ int main(int argc, char** argv) {
     }
 
     CXCursor root_cursor = clang_getTranslationUnitCursor(tu);
-    traverse_ast(root_cursor, 0, [](CXCursor cursor, int depth) {
-        for (int i = 0; i < depth; ++i)
-            std::cout << "  ";
-        CXString cursor_kind_name = clang_getCursorKindSpelling(clang_getCursorKind(cursor));
-        CXString cursor_spelling = clang_getCursorSpelling(cursor);
-        std::cout << clang_getCString(cursor_kind_name) << " " << clang_getCString(cursor_spelling) << std::endl;
-        clang_disposeString(cursor_kind_name);
-        clang_disposeString(cursor_spelling);
-    });
+
+    print_ast(root_cursor, 0);
+
+    for (const auto& var_and_context : variables_and_context) {
+        std::cout << "Variable: " << var_and_context.first << std::endl;
+        std::cout << "Context:" << std::endl << var_and_context.second << std::endl;
+    }
 
     clang_disposeTranslationUnit(tu);
     clang_disposeIndex(index);
